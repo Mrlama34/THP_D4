8591e93e385104f2755bb982e614a643a7491c27
LeoDeb <leo.deboin@gmail.com>
2018-04-05 19:12:52 +0200

    Warmup done

---
+++ main.js
@@ -0,0 +1,9 @@
+let mainpage = document.getElementById('mainpage');
+mainpage.style.display='none';
+
+ function myFunction()
+
+{
+   mainpage.style.display='flex';
+   bouton3.style.display='none';
+}
